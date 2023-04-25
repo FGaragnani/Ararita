10f9bc368de6e6d931de316e46245ec162ef272e
FGaragnani <filippogaragnani1@gmail.com>
2023-04-25 13:26:43 +0200

    Removed Android support
    Now should work more smoothly

--- desktop/src/com/ararita/game/DesktopLauncher.java
+++ desktop/src/com/ararita/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("ararita");
+		config.setTitle("Ararita");
 		new Lwjgl3Application(new AraritaGame(), config);
 	}
 }
