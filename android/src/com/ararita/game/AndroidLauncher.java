97a07e8067ddd1e125aa594dbd004e4e77f43586
FGaragnani <filippogaragnani1@gmail.com>
2023-04-25 11:25:28 +0200

    Added LibGDX
    Recreated the repo from almost scratch

---
+++ android/src/com/ararita/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ararita.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ararita.game.AraritaGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AraritaGame(), config);
+	}
+}
