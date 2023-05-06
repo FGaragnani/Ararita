01a517503f47b8210927c51812b9c3d83a176b16
FGaragnani <filippogaragnani1@gmail.com>
2023-05-06 09:53:54 +0200

    Commented Inventory code

--- core/src/com/ararita/game/items/Inventory.java
+++ core/src/com/ararita/game/items/Inventory.java
@@ -15,10 +15,16 @@ public class Inventory {
     final int MAX_INVENTORY_SPACE;
     final double RESELL_MULTIPLIER;
 
+    /**
+     * Inventory constructor. All data is fetched from the global manager.
+     *
+     * @throws IOException If the file cannot be opened or read.
+     */
+
     public Inventory() throws IOException {
-        Global.getMapJSON(Global.globalSets, "inventory").forEach((key, value) -> {
+        Global.getInventory().forEach((key, value) -> {
             try {
-                items.put(Global.getItem(key), (int) value);
+                items.put(Global.getItem(key), value);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -37,6 +43,11 @@ public class Inventory {
         updateItems();
     }
 
+    /**
+     * The items are overwritten in the global manager.
+     *
+     * @throws IOException If the file cannot be read or written upon.
+     */
     public void updateItems() throws IOException {
         JSONObject jsonGlobal = Global.getJSON(Global.globalSets);
         jsonGlobal.remove("inventory");
@@ -54,14 +65,35 @@ public class Inventory {
         Global.setMoney(money);
     }
 
+    /**
+     * Returns the size of the inventory, as a sum of its integer values.
+     *
+     * @return The inventory's size.
+     */
     public int inventorySize() {
         return items.entrySet().stream().flatMapToInt((entry) -> IntStream.of((entry.getValue()))).sum();
     }
 
+    /**
+     * Determines if the user can buy N copies of the same item.
+     *
+     * @param item The item to buy.
+     * @param n The number of items to buy.
+     *
+     * @return True, if those can be bought.
+     */
     public boolean canBuy(Item item, int n) {
         return (inventorySize() + n <= MAX_INVENTORY_SPACE && ((item.getPrice() * n) <= getMoney()) && n >= 0);
     }
 
+    /**
+     * Buys N copies of the same item.
+     *
+     * @param item The item to buy.
+     * @param n The number of items to buy.
+     *
+     * @throws IOException If the file cannot be written upon.
+     */
     public void buy(Item item, int n) throws IOException {
         if (canBuy(item, n)) {
             if (items.containsKey(item)) {
@@ -74,20 +106,35 @@ public class Inventory {
         }
     }
 
+    /**
+     * Determines if you can sell N copies of the same item.
+     *
+     * @param item The item to sell.
+     * @param n The number of items to sell.
+     *
+     * @return True, if those can be sold.
+     */
     public boolean canSell(Item item, int n) {
         return items.get(item) >= n;
     }
 
-    public void sell(Item item, int n) throws IOException{
-        if(canSell(item, n)){
-            if(items.get(item) == n){
+    /**
+     * N copies of the same item are sold.
+     *
+     * @param item The item to sell.
+     * @param n How many items to sell.
+     *
+     * @throws IOException If the file cannot be written upon.
+     */
+    public void sell(Item item, int n) throws IOException {
+        if (canSell(item, n)) {
+            if (items.get(item) == n) {
                 items.remove(item);
             } else {
                 items.put(item, items.get(item) - n);
             }
-            setMoney(getMoney() - (int)(item.getPrice() * n * RESELL_MULTIPLIER));
+            setMoney(getMoney() - (int) (item.getPrice() * n * RESELL_MULTIPLIER));
             updateItems();
         }
     }
-
 }
