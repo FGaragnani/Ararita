449cfab5f4346f81d0be1f8325a193eadee7c7c3
FGaragnani <filippogaragnani1@gmail.com>
2023-05-06 13:08:28 +0200

    Fullscreen mode set by default

--- desktop/src/com/ararita/game/DesktopLauncher.java
+++ desktop/src/com/ararita/game/DesktopLauncher.java
@@ -1,7 +1,11 @@
 package com.ararita.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import java.io.IOException;
+import java.util.List;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
@@ -10,6 +14,7 @@ public class DesktopLauncher {
         config.setTitle("Ararita");
         config.setWindowIcon("General/Ararita.jpg");
         config.setWindowedMode(1920, 1080);
+        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 
         new Lwjgl3Application(new Ararita(), config);
     }
